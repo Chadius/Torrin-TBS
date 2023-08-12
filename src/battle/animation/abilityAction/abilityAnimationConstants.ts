80cff9ae2a5774a3bead6e891ec9bdb6d4f6c03c
Chad Serrant <therealchadius@gmail.com>
2023-08-11 20:21:28 -0400

    Add Attacking sign for the Weapon Icon.

---
+++ src/battle/animation/abilityAction/abilityAnimationConstants.ts
@@ -0,0 +1,4 @@
+export const ACTION_ANIMATION_ATTACK_TIME = 1500;
+export const ACTOR_X_SCREEN_COLUMN_START= 1;
+export const ACTOR_X_SCREEN_COLUMN_END= 6;
+export const ActionAnimationFontColor = [0, 5, 10];
