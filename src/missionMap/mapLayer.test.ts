f4e38845dd4fa6dafcbf502f656cc971a68beb0c
Chad Serrant <therealchadius@gmail.com>
2023-12-19 09:52:49 -0500

    Add MapLayer object to track information on a map.

---
+++ src/missionMap/mapLayer.test.ts
@@ -0,0 +1,156 @@
+import {MissionMap} from "./missionMap";
+import {TerrainTileMap} from "../hexMap/terrainTileMap";
+import {MapLayer, MapLayerHelper} from "./mapLayer";
+
+describe('MapLayer', () => {
+    it('can generate a layer based on a given map and initial value', () => {
+        const missionMap: MissionMap = new MissionMap({
+            terrainTileMap: new TerrainTileMap({
+                movementCost: [
+                    "1 1 2 1 2 ",
+                    " 1 x - 2 1 ",
+                ]
+            })
+        });
+
+        const mapLayerFilledWithFalse: MapLayer = MapLayerHelper.new({
+            map: missionMap,
+            initialValue: false,
+        });
+
+        expect(mapLayerFilledWithFalse.widthOfWidestRow).toBe(5);
+        expect(mapLayerFilledWithFalse.numberOfRows).toBe(2);
+
+        [0, 1, 2, 3, 4].forEach(r => {
+            [0, 1].forEach(q => {
+                expect(mapLayerFilledWithFalse.valueByLocation[q][r]).toBe(false);
+            });
+        })
+    });
+
+    it('can generate a layer based on a given map and value filling function', () => {
+        const missionMap: MissionMap = new MissionMap({
+            terrainTileMap: new TerrainTileMap({
+                movementCost: [
+                    "1 1 2 1 2 ",
+                    " 1 x - 2 1 ",
+                ]
+            })
+        });
+
+        const initialValueFill = (q: number, r: number): boolean | number => {
+            return q + r;
+        }
+
+        const mapLayerFilledWithSumOfCoordinates: MapLayer = MapLayerHelper.new({
+            map: missionMap,
+            initialValue: initialValueFill,
+        });
+
+        expect(mapLayerFilledWithSumOfCoordinates.widthOfWidestRow).toBe(5);
+        expect(mapLayerFilledWithSumOfCoordinates.numberOfRows).toBe(2);
+
+        [0, 1, 2, 3, 4].forEach(r => {
+            [0, 1].forEach(q => {
+                expect(mapLayerFilledWithSumOfCoordinates.valueByLocation[q][r]).toBe(q + r);
+            });
+        })
+    });
+
+    it('can generate a layer based on a given map and use undefined if given', () => {
+        const missionMap: MissionMap = new MissionMap({
+            terrainTileMap: new TerrainTileMap({
+                movementCost: [
+                    "1 1 2 1 2 ",
+                    " 1 x - 2 1 ",
+                ]
+            })
+        });
+
+        const mapLayerFilledWithUndefined: MapLayer = MapLayerHelper.new({
+            map: missionMap,
+            initialValue: undefined,
+        });
+
+        expect(mapLayerFilledWithUndefined.widthOfWidestRow).toBe(5);
+        expect(mapLayerFilledWithUndefined.numberOfRows).toBe(2);
+
+        [0, 1, 2, 3, 4].forEach(r => {
+            [0, 1].forEach(q => {
+                expect(mapLayerFilledWithUndefined.valueByLocation[q][r]).toBe(undefined);
+            });
+        })
+    });
+
+    it('can set the value of an individual tile on the layer', () => {
+        const missionMap: MissionMap = new MissionMap({
+            terrainTileMap: new TerrainTileMap({
+                movementCost: [
+                    "1 1 2 1 2 ",
+                    " 1 x - 2 1 ",
+                ]
+            })
+        });
+        const mapLayerFilledWithFalse: MapLayer = MapLayerHelper.new({
+            map: missionMap,
+            initialValue: false,
+        });
+        MapLayerHelper.setValueOfLocation({mapLayer: mapLayerFilledWithFalse, q: 1, r: 2, value: true});
+        expect(mapLayerFilledWithFalse.valueByLocation[1][2]).toBe(true);
+    });
+
+    it('throws an error if setting a location that is out of bounds', () => {
+        const missionMap: MissionMap = new MissionMap({
+            terrainTileMap: new TerrainTileMap({
+                movementCost: [
+                    "1 1 2 1 2 ",
+                    " 1 x - 2 1 ",
+                ]
+            })
+        });
+        const mapLayerFilledWithFalse: MapLayer = MapLayerHelper.new({
+            map: missionMap,
+            initialValue: false,
+        });
+
+        const shouldThrowError = () => {
+            MapLayerHelper.setValueOfLocation({mapLayer: mapLayerFilledWithFalse, q: 9001, r: -3, value: true});
+        }
+
+        expect(() => {
+            shouldThrowError();
+        }).toThrow("out of bounds");
+    });
+
+    it('knows when a location is out of bounds', () => {
+        const missionMap: MissionMap = new MissionMap({
+            terrainTileMap: new TerrainTileMap({
+                movementCost: [
+                    "1 1 2 1 2 ",
+                    " 1 x - 2 1 ",
+                ]
+            })
+        });
+        const mapLayerFilledWithFalse: MapLayer = MapLayerHelper.new({
+            map: missionMap,
+            initialValue: false,
+        });
+        [0, 1, 2, 3, 4].forEach(r => {
+            [0, 1].forEach(q => {
+                expect(MapLayerHelper.outOfBounds({mapLayer: mapLayerFilledWithFalse, q, r})).toBe(false);
+            });
+        });
+        expect(MapLayerHelper.outOfBounds({mapLayer: mapLayerFilledWithFalse, q: -1, r: 0})).toBe(true);
+        expect(MapLayerHelper.outOfBounds({
+            mapLayer: mapLayerFilledWithFalse,
+            q: -1,
+            r: mapLayerFilledWithFalse.widthOfWidestRow
+        })).toBe(true);
+        expect(MapLayerHelper.outOfBounds({mapLayer: mapLayerFilledWithFalse, q: 0, r: -1})).toBe(true);
+        expect(MapLayerHelper.outOfBounds({
+            mapLayer: mapLayerFilledWithFalse,
+            q: mapLayerFilledWithFalse.numberOfRows,
+            r: 0
+        })).toBe(true);
+    });
+});
---
+++ src/missionMap/mapLayer.ts
@@ -0,0 +1,67 @@
+import {MissionMap} from "./missionMap";
+
+export interface MapLayer {
+    numberOfRows: number;
+    widthOfWidestRow: number;
+    valueByLocation: {
+        [q: number]: {
+            [r: number]: boolean | number;
+        }
+    }
+}
+
+export type MapCoordinateValueGenerator = (q: number, r: number) => boolean | number;
+
+export const MapLayerHelper = {
+    new: ({map, initialValue}: {
+        map: MissionMap;
+        initialValue: boolean | number | undefined | MapCoordinateValueGenerator
+    }): MapLayer => {
+        const newMapLayer: MapLayer = {
+            numberOfRows: map.terrainTileMap.getDimensions().numberOfRows,
+            widthOfWidestRow: map.terrainTileMap.getDimensions().widthOfWidestRow,
+            valueByLocation: {},
+        }
+
+        for (let q = 0; q < newMapLayer.numberOfRows; q++) {
+            newMapLayer.valueByLocation[q] = {};
+            for (let r = 0; r < newMapLayer.widthOfWidestRow; r++) {
+                if (
+                    initialValue === undefined
+                    || ["boolean", "number"].includes(typeof initialValue)
+                ) {
+                    newMapLayer.valueByLocation[q][r] = initialValue as number | boolean | undefined;
+                } else {
+                    const initialValueFunction = initialValue as MapCoordinateValueGenerator;
+                    newMapLayer.valueByLocation[q][r] = initialValueFunction(q, r);
+                }
+            }
+        }
+
+        return newMapLayer;
+    },
+    setValueOfLocation: ({mapLayer, q, r, value}: {
+        mapLayer: MapLayer,
+        q: number;
+        r: number;
+        value: boolean | number | undefined
+    }) => {
+        if (outOfBounds({mapLayer, q, r})) {
+            throw new Error(`(${q}, ${r}) is out of bounds, must be within (${mapLayer.numberOfRows}, ${mapLayer.widthOfWidestRow})`);
+        }
+
+        mapLayer.valueByLocation[q][r] = value;
+    },
+    outOfBounds: ({mapLayer, q, r}: { mapLayer: MapLayer, q: number; r: number }): boolean => {
+        return outOfBounds({mapLayer, q, r});
+    }
+}
+
+const outOfBounds = ({mapLayer, q, r}: { mapLayer: MapLayer, q: number; r: number; }): boolean => {
+    return (
+        q < 0
+        || q >= mapLayer.numberOfRows
+        || r < 0
+        || r >= mapLayer.widthOfWidestRow
+    );
+}
