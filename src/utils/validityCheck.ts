c87f65317916da1c6e3880f69846ee79e73ea8cd
Chad Serrant <therealchadius@gmail.com>
2024-01-30 11:31:48 -0500

    Add getValidValueOrDefault convenience function.

--- src/utils/validityCheck.ts
+++ src/utils/validityCheck.ts
@@ -3,3 +3,10 @@ const notFound = [NaN, false, undefined, null];
 export const isValidValue = (value: any): boolean => {
     return !notFound.includes(value);
 }
+
+export const getValidValueOrDefault = <T>(value: T, defaultValue: T) => {
+    return isValidValue(value)
+        ? value
+        : defaultValue
+        ;
+}
